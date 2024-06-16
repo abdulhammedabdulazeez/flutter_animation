4b5f166129fd69050f4fe8ccfeb407d38dcffb35
Abdulhammed Abdulazeez <a.abdulazee@alustudent.com>
2024-06-17 00:12:09 +0200

    complete animations

--- app_animation/lib/main.dart
+++ app_animation/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(_MyApp());
+  runApp(const _Myapp());
 }
 
-class _MyApp extends StatelessWidget {
+class _Myapp extends StatefulWidget {
+  const _Myapp({super.key});
+
+  @override
+  State<_Myapp> createState() => __MyappState();
+}
+
+class __MyappState extends State<_Myapp> {
   var _color = Colors.yellow;
   var _height = 200.0;
   var _width = 200.0;
@@ -22,13 +29,14 @@ class _MyApp extends StatelessWidget {
                   width: _width,
                   height: _height),
               ElevatedButton(
-                  child: const Text("Animate!"),
-                  onPressed: () {
-                    _color = Colors.red;
-                    _height = 400.0;
-                    _width = 400.0;
-                    setState(() {});
-                  })
+                child: const Text("Animate!"),
+                onPressed: () {
+                  _color = Colors.red;
+                  _height = 400.0;
+                  _width = 400.0;
+                  setState(() {});
+                }
+              ),
             ],
           ),
         ),
